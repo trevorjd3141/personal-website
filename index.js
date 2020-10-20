7c2ceca2c9e9135812f44cdfb09f598b6eee2962
Trevor Dalton <trevorjd3141@gmail.com>
2020-10-20 15:46:20 -0600

    Bars work upon updating bubbles.

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const activeSkills = {};
 
 function createShapeBars (data) {
+    d3.select('#skills').select('svg').selectAll('rect').remove();
     d3.select('#skills').select('svg').selectAll('rect').data(data)
-        .join('rect')
-        .attr('height', 12)
-        .attr('width', d => d.value)
+        .join('rect').filter(d => activeSkills[d.name])
+        .attr('height', 20)
+        .attr('width', d => d.value*3)
         .attr('transform', (d,i) => "translate(0, "+ i*32 + ")");
 }
 
 $(document).ready(function(){
     d3.csv('/data/skills.csv').then(skills => {
         skills.forEach(skill => {
-            activeSkills[skill.name] = false;
+            if (skill.name !== 'Python') {
+                activeSkills[skill.name] = true;
+            } else {
+                activeSkills[skill.name] = false;
+            }
         })
         
         d3.select('#about-bubbles').selectAll('li').data(skills)
             .join('li')
             .text(d => d.name)
+            .attr('class', (d,i,g) => {
+                if (d.name !== 'Python') {
+                    return 'active';
+                }
+            })
             .on('click', (d,i,g) => {
                 // Add the class 'active' to active buttons
                 let currBubble = d3.select(g[i]);
@@ -26,5 +36,7 @@ $(document).ready(function(){
 
                 createShapeBars(skills);
             });
+
+        createShapeBars(skills);
     })
 });
