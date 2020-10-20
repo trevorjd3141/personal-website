aebb5baa49cb2d4945d677f11e9a29f89daa9ff1
Trevor Dalton <trevorjd3141@gmail.com>
2020-10-20 02:43:49 -0600

    Remove skill tags from HTML and dynamically load in from csv and then create bars in an svg.

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+const activeSkills = {};
+
+function createShapeBars (data) {
+    d3.select('#skills').select('svg').selectAll('rect').data(data)
+        .join('rect')
+        .attr('height', 12)
+        .attr('width', d => d.value)
+        .attr('transform', (d,i) => "translate(0, "+ i*32 + ")");
+}
+
 $(document).ready(function(){
-    let ul = document.getElementById("about-bubbles");
-    let aboutBubbles = ul.getElementsByTagName("li");
-    for(li of aboutBubbles){
-      li.addEventListener('click', function(){
-        if(this.classList.contains('active')){
-          this.classList.remove("active");
-        } else {
-          this.classList.add("active");
-        }
-      })
-    }
+    d3.csv('/data/skills.csv').then(skills => {
+        skills.forEach(skill => {
+            activeSkills[skill.name] = false;
+        })
+        
+        d3.select('#about-bubbles').selectAll('li').data(skills)
+            .join('li')
+            .text(d => d.name)
+            .on('click', (d,i,g) => {
+                // Add the class 'active' to active buttons
+                let currBubble = d3.select(g[i]);
+                let currStatus = currBubble.classed('active');
+                currBubble.classed('active', !currStatus);
+                activeSkills[d.name] = !currStatus;
+
+                createShapeBars(skills);
+            });
+    })
 });
