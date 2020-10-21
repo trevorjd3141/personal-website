5c57ac08fd6a3ebf3ee42806cd9dc5e306c66b19
Trevor Dalton <trevorjd3141@gmail.com>
2020-10-21 01:44:39 -0600

    Bars animate movement when activated

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const activeSkills = {};
+const rectSpacing = 30;
+const animationLength = 700;
 
 function createShapeBars (data) {
-    d3.select('#skills').select('svg').selectAll('rect').remove();
-    d3.select('#skills').select('svg').selectAll('rect').data(data)
-        .join('rect').filter(d => activeSkills[d.name])
+    d3.select('#skills').select('svg')
+        .append('g')
+        .attr('id', 'rectGroup');
+
+    d3.select('#rectGroup').selectAll('rect').data(data)
+        .join('rect')
+        .attr('id', d => d.id)
         .attr('height', 20)
         .attr('width', 0)
-        .attr('transform', (d,i) => "translate(0, "+ i*32 + ")")
+        .attr('transform', (d,i) => "translate(0, " + i*rectSpacing + ")")
         .transition()
         .duration((d,i) => 400 + i*100)
         .attr('width', d => d.value*3);
@@ -14,30 +19,55 @@ function createShapeBars (data) {
 
 $(document).ready(function(){
     d3.csv('/data/skills.csv').then(skills => {
-        skills.forEach(skill => {
-            if (skill.name !== 'Python') {
-                activeSkills[skill.name] = true;
-            } else {
-                activeSkills[skill.name] = false;
-            }
-        })
         
         d3.select('#about-bubbles').selectAll('li').data(skills)
             .join('li')
             .text(d => d.name)
-            .attr('class', (d,i,g) => {
-                if (d.name !== 'Python') {
-                    return 'active';
-                }
-            })
             .on('click', (d,i,g) => {
-                // Add the class 'active' to active buttons
-                let currBubble = d3.select(g[i]);
-                let currStatus = currBubble.classed('active');
-                currBubble.classed('active', !currStatus);
-                activeSkills[d.name] = !currStatus;
+                let currStatus = d3.select(g[i]).classed('active');
 
-                createShapeBars(skills);
+                // Activate the appropriate skill bubble
+                d3.select('#about-bubbles').selectAll('li')
+                    .classed('active', false);
+                d3.select(g[i]).classed('active', !currStatus);
+
+                // Activate the appropriate rectangle
+                d3.select('#rectGroup').selectAll('rect')
+                    .classed('active', false);
+                d3.select("#" + d.id).classed('active', !currStatus);
+
+                // Create spacing for rectangles
+                if (!currStatus) {
+                    d3.select('#rectGroup').selectAll('rect')
+                        .transition()
+                        .duration(animationLength)
+                        .attr('transform', (rectD, rectI) => {
+                            // Shift rectangles when one is activated
+                            if (rectI < i) {
+                                // Rectangles above the activated
+                                // one aren't shifted
+                                let yPos = rectI*rectSpacing;
+                                return "translate(0, " + yPos + ")";
+                            } else if (rectI === i) {
+                                // Activated rectangle gets shifted down
+                                // By one "stretch"
+                                let yPos = i ? rectI*rectSpacing + rectSpacing : rectI*rectSpacing;
+                                return "translate(0, " + yPos + ")";
+                            } else {
+                                // Rectangles below the activated one
+                                // Get shifted down by two "stretches"
+                                let yPos = i ? rectI*rectSpacing + rectSpacing*2 : rectI*rectSpacing + rectSpacing;
+                                return "translate(0, " + yPos + ")";                                    
+                            }
+                        });
+                } else {
+                    d3.select('#rectGroup').selectAll('rect')
+                        .transition()
+                        .duration(animationLength)
+                        .attr('transform', (rectD, rectI) => {
+                            return "translate(0, " + rectI*rectSpacing + ")";
+                        });
+                }
             });
 
         createShapeBars(skills);
